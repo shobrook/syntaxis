5fa8d521341a52cdea25bce41a0a87e367e966a2
Jonathan Shobrook <shobrookj@gmail.com>
2018-07-15 00:10:01 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from codecs import open
+
+setup(
+    name="saplings",
+    description="Library of generic algorithms for working with abstract syntax trees",
+    version="v1.0.0",
+    packages=["saplings"],
+    python_requires=">=3",
+    url="https://github.com/shobrook/saplings",
+    author="shobrook",
+    author_email="shobrookj@gmail.com",
+    keywords=["saplings", "abstract-syntax-trees", "ast", "trees", "tree-algorithms"],
+    license="MIT"
+)
