569e2af0c6c01c0d1e8f3a3cf68f7afc37fc4cf8
Jonathan Shobrook <shobrookj@gmail.com>
2018-07-21 17:00:27 -0400

    Added PackageTree

--- saplings/saplings.py
+++ saplings/saplings.py
@@ -92,7 +92,158 @@ class IdentifierNode(object):
 
 class PackageTree(ast.NodeVisitor):
     def __init__(self, tree):
-        pass
+        self._context_stack = ["global"] # Keeps track of current context
+        self._module_forest = []
+
+        self.visit(tree)
+
+    ## Helper Methods ##
+
+    def _update_context(self, context_name, node):
+        self._context_stack.append(context_name)
+        self.generic_visit(node)
+        self._context_stack.pop()
+
+    def _recursive_alias_search(self, node, target_id):
+        for alias in node.aliases:
+            if alias.id == target_id: # and self._context_stack[-1] in alias.contexts
+                return node
+
+        for child in node.children:
+            return self._recursive_alias_search(child, target_id)
+
+    ## Context Managers ##
+
+    def visit_Global(self, node):
+        self.generic_visit(node) # TODO
+
+    def visit_Nonlocal(self, node):
+        self.generic_visit(node) # TODO
+
+    def visit_ClassDef(self, node):
+        self._update_context("class", node)
+
+    def visit_FunctionDef(self, node):
+        self._update_context("function", node)
+
+    def visit_AsyncFunctionDef(self, node):
+        self._update_context("function", node)
+
+    def visit_Lambda(self, node):
+        self._update_context("function", node)
+
+    ## Import Visitors ##
+
+    def visit_Import(self, node):
+        for alias in node.names:
+            alias_id = alias.asname if alias.asname else alias.name
+            current_context = self._context_stack[-1]
+            module_exists = False
+
+            for module_node in self._module_forest:
+                if alias.name == module_node.id: # Module tree already in forest
+                    module_exists = True
+                    alias_exists = False
+
+                    for module_alias in module_node.aliases:
+                        if module_alias.id == alias_id: # Alias already exists
+                            alias_exists = True
+                            if current_context in module_alias.contexts: # Alias already exists in current context
+                                break
+                            else: # Alias exists but not in current context
+                                module_alias.add_context(current_context)
+
+                    module_node.increment_count()
+                    if not alias_exists: # Alias doesn't exist
+                        module_node.add_alias(AliasNode(id=alias_id, contexts={current_context}))
+
+                    break
+
+            if not module_exists: # Module tree doesn't exist
+                self._module_forest.append(IdentifierNode(id=alias.name, aliases=[AliasNode(id=alias_id, contexts={current_context})]))
+
+        self.generic_visit(node)
+
+    def visit_ImportFrom(self, node):
+        module_exists = False
+        current_context = self._context_stack[-1]
+
+        for module_node in self._module_forest:
+            if node.module == module_node.id: # Module tree already in forest
+                module_exists = True
+
+                for alias in node.names:
+                    alias_exists = False
+                    alias_id = alias.asname if alias.asname else alias.name
+
+                    for child in module_node.children:
+                        if alias.name == child.id: # Module feature already exists
+                            for child_alias in child.aliases:
+                                if child_alias.id == alias_id: # Alias already exists
+                                    alias_exists = True
+                                    if current_context in child_alias.contexts: # Alias exists in current context
+                                        break
+                                    else:
+                                        child_alias.add_context(current_context)
+
+                            child.increment_count()
+                            if not alias_exists: # Alias doesn't exist
+                                child.add_alias(AliasNode(id=alias_id, contexts={current_context}))
+                                alias_exists = True
+
+                            break
+
+                    if not alias_exists: # Module feature doesn't exist
+                        module_node.add_child(IdentifierNode(id=alias.name, aliases=[AliasNode(id=alias_id, contexts={current_context})]))
+
+                module_node.increment_count()
+                break
+
+        if not module_exists: # Module doesn't exist
+            module_node = IdentifierNode(id=node.module)
+
+            for alias in node.names:
+                alias_id = alias.asname if alias.asname else alias.name
+                module_node.add_child(IdentifierNode(id=alias.name, aliases=[AliasNode(id=alias_id, contexts={current_context})]))
+
+            self._module_forest.append(module_node)
+
+        self.generic_visit(node)
+
+    ## Other Visitors ##
+
+    def visit_Call(self, node):
+        if isinstance(node.func, ast.Name):
+            for module_node in self._module_forest:
+                func_node = self._recursive_alias_search(module_node, node.func.id)
+                if func_node != None:
+                    func_node.increment_count()
+        elif isinstance(node.func, ast.Attribute):
+            for module_node in self._module_forest:
+                if isinstance(node.func.value, ast.Name):
+                    func_node = self._recursive_alias_search(module_node, node.func.value.id)
+                    if func_node != None:
+                        attr_node = self._recursive_alias_search(module_node, node.func.attr)
+                        if attr_node != None:
+                            attr_node.increment_count()
+                        else:
+                            func_node.add_child(IdentifierNode(
+                                id=node.func.attr,
+                                aliases=[AliasNode(
+                                    id=node.func.attr,
+                                    contexts={self._context_stack[-1]}
+                                )]
+                            ))
+
+        self.generic_visit(node)
+
+    def visit_Assign(self, node):
+        self.generic_visit(node) # TODO
+
+    ## Public Methods ##
+
+    def to_dict(self):
+        return [module_node.to_dict() for module_node in self._module_forest]
 
 
 ###########
@@ -100,10 +251,9 @@ class PackageTree(ast.NodeVisitor):
 ###########
 
 
-class Harvester(ast.NodeVisitor): # ast.NodeVisitor
+class Harvester(ast.NodeVisitor): # ast.NodeTransformer
     def __init__(self, tree):
         self._root = tree
-        self.__context_stack = ["global"] # Keeps track of current context
 
     ## Overloaded Methods ##
 
@@ -113,13 +263,13 @@ class Harvester(ast.NodeVisitor): # ast.NodeVisitor
         elif not self.__nodes or any(isinstance(node, n_type) for n_type in self.__nodes):
             self.__retrieved.append(node)
             self.__freq_map[type(node).__name__] += 1
+            node = ast.copy_location(self.__transformer(node), node)
 
         ast.NodeVisitor.generic_visit(self, node)
 
     ## Private Methods ##
 
     def __reset_fields(self):
-        self.__context_stack = ["global"]
         self.__nodes, self.__skip = [], []
         self.__retrieved = []
         self.__freq_map = defaultdict(lambda: 0, {})
@@ -163,7 +313,12 @@ class Harvester(ast.NodeVisitor): # ast.NodeVisitor
         return dict(self.__freq_map)
 
     def transform(nodes=[], transformer=lambda node: node, skip=[]):
-        pass
+        self.__reset_fields()
+        self.__transformer = transformer
+        self.__nodes, self.__skip = nodes, skip
+        self.visit(self._root)
+
+        return self._root
 
     def get_halstead(metric_name) -> float:
         pass
@@ -174,6 +329,12 @@ class Harvester(ast.NodeVisitor): # ast.NodeVisitor
     def get_pkg_tree(pkg_names=[]):
         pass
 
+    ## Properties ##
+
+    @property
+    def _root(self):
+        return self._root
+
 
 #########
 # TESTING
@@ -181,9 +342,13 @@ class Harvester(ast.NodeVisitor): # ast.NodeVisitor
 
 
 if __name__ == "__main__":
-    my_file = open("harvester.py", 'r').read()
+    my_file = open("py_ast.py", 'r').read()
     my_ast = ast.parse(my_file)
-    my_harvester = Harvester(my_ast)
 
-    print(my_harvester.find(nodes=[ast.For]))
-    print(my_harvester.get_freq_map(nodes=[ast.Call, ast.For]))
+    #my_harvester = Harvester(my_ast)
+    my_pkg_forest = PackageTree(my_ast)
+
+    #print(my_harvester.find(nodes=[ast.For]))
+    #print(my_harvester.get_freq_map(nodes=[ast.Call, ast.For]))
+
+    print(my_pkg_forest.to_dict())
