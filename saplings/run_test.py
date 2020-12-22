f23f65e6bbed2dd5b5d7c499546607ca22fbd1c8
shobrook <shobrookj@gmail.com>
2020-12-22 03:20:13 -0500

    Deleted test files

--- saplings/run_test.py
+++
@@ -1,9 +0,0 @@
-from transducer import Saplings
-import ast
-from pprint import pprint
-
-my_program = open("test_input.py", "r").read()
-program_ast = ast.parse(my_program)
-my_saplings = Saplings(program_ast)
-
-pprint(my_saplings.trees())
