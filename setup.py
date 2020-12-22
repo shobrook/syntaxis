a176a0c8c2da06141ed1e605cf6903304fbc90d5
shobrook <shobrookj@gmail.com>
2020-12-22 02:46:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
+import sys
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 from codecs import open
 
+if sys.version_info[:3] < (3, 0, 0):
+    print("Requires Python 3 to run.")
+    sys.exit(1)
+
+with open("README.md", encoding="utf-8") as file:
+    readme = file.read()
+
 setup(
     name="saplings",
-    description="Library of algorithms and data structures for working with abstract syntax trees",
-    version="v2.0.5",
+    description="Static analysis tool for Python",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    version="v4.0.0",
     packages=["saplings"],
     python_requires=">=3",
     url="https://github.com/shobrook/saplings",
     author="shobrook",
     author_email="shobrookj@gmail.com",
-    keywords=["saplings", "abstract-syntax-trees", "ast", "trees", "tree-algorithms"],
+    # classifiers=[],
+    install_requires=[],
+    keywords=["ast", "object", "hierarchy", "tree", "static", "analysis", "dependency", "module"],
     license="MIT"
 )
